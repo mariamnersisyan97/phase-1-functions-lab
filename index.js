95ab0b64e547079611b09579eb099d6cfa28285a
Mariam Nersisyan <mariamnersisyan97@gmail.com>
2022-02-06 13:42:05 -0500

    commit tree 477156d0a6071c03dcec22690937c138ea680c84

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(start) {
+    if (start > 42)
+        return start - 42;
+    else (start < 42)
+        return 42 - start;
+}
+
+function distanceFromHqInFeet(startFeet) {
+    if (startFeet > 42)
+        return (startFeet - 42) * 264;
+    else (startFeet < 42)
+    return (42 - startFeet) * 264;
+}
+
+function distanceTravelledInFeet(starting, destination) {
+    if (starting < destination)
+        return (destination - starting) * 264;
+    else (starting > destination)
+        return (starting - destination) * 264;
+}
+
+function calculatesFarePrice(start, destination) {
+    let blockDistance = Math.abs(start - destination);
+    let distanceTravelled = (blockDistance * 264);
+    let flatFare = (distanceTravelled - 400)* 0.02;
+    if (distanceTravelled < 400){
+        return 0;
+    } else if (distanceTravelled > 400 && distanceTravelled <2000){
+        return flatFare;
+    } else if (distanceTravelled > 2000 && distanceTravelled <= 2500){
+        return 25;
+    } else if (distanceTravelled > 2500){
+        return 'cannot travel that far';
+    }
+};
+
